75dc4e74cd221a956028e26ecace155b527eac59
BernardoS <bernardo.sfs27@gmail.com>
2026-05-12 08:22:39 -0300

    feat: add `ProfileCreated` event

---
+++ src/Application/Events/Event.cs
@@ -0,0 +1,7 @@
+﻿namespace CustomerProfileService.Application.Events;
+
+public abstract class Event
+{
+    public string Description { get; set; }
+    public DateTime Timestamp { get; set; }
+}
---
+++ src/Application/Events/ProfileCreatedEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomerProfileService.Application.Events;
+
+public class ProfileCreatedEvent:Event
+{
+    public Guid CustomerId { get; private set; }
+    public Guid ProfileId { get; private set; }
+
+    public ProfileCreatedEvent(Guid  customerId, Guid profileId)
+    {
+        CustomerId = customerId;
+        ProfileId = profileId;
+        this.Timestamp = DateTime.Now;
+        this.Description = $"Perfil do usuário {customerId} criado com sucesso!";
+    }
+    
+}
