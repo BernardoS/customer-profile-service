7a0ae3603ec7dc527b27ff99e92c922003d6857c
BernardoS <bernardo.sfs27@gmail.com>
2026-05-18 11:37:31 -0300

    feat: alter relation between profile and customer

---
+++ src/Infrastructure/Persistence/Migrations/20260501164729_IncludeCustomerIdInProfile.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace customer_profile_service.src.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncludeCustomerIdInProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Profiles_ProfileId",
+                table: "Customers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_QuestionForm_QuestionFormId",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_ProfileId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileId",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "QuestionFormId",
+                table: "Questions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CustomerId",
+                table: "Profiles",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_CustomerId",
+                table: "Profiles",
+                column: "CustomerId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Profiles_Customers_CustomerId",
+                table: "Profiles",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_QuestionForm_QuestionFormId",
+                table: "Questions",
+                column: "QuestionFormId",
+                principalTable: "QuestionForm",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Profiles_Customers_CustomerId",
+                table: "Profiles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_QuestionForm_QuestionFormId",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Profiles_CustomerId",
+                table: "Profiles");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "Profiles");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "QuestionFormId",
+                table: "Questions",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProfileId",
+                table: "Customers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_ProfileId",
+                table: "Customers",
+                column: "ProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Profiles_ProfileId",
+                table: "Customers",
+                column: "ProfileId",
+                principalTable: "Profiles",
+                principalColumn: "ProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_QuestionForm_QuestionFormId",
+                table: "Questions",
+                column: "QuestionFormId",
+                principalTable: "QuestionForm",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ src/Infrastructure/Persistence/Migrations/20260506004341_ChangeCustomerProfileRelation.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace customer_profile_service.src.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCustomerProfileRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FormAnswers_Customers_CustomerId",
+                table: "FormAnswers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_FormAnswers_QuestionForm_AnsweredFormId",
+                table: "FormAnswers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_FormAnswers_QuestionOptions_AnsweredQuestionOptionId",
+                table: "FormAnswers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_FormAnswers_Questions_AnsweredQuestionId",
+                table: "FormAnswers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Profiles_CustomerId",
+                table: "Profiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FormAnswers_AnsweredFormId",
+                table: "FormAnswers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FormAnswers_AnsweredQuestionId",
+                table: "FormAnswers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FormAnswers_AnsweredQuestionOptionId",
+                table: "FormAnswers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FormAnswers_CustomerId",
+                table: "FormAnswers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_CustomerId",
+                table: "Profiles",
+                column: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Profiles_CustomerId",
+                table: "Profiles");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_CustomerId",
+                table: "Profiles",
+                column: "CustomerId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormAnswers_AnsweredFormId",
+                table: "FormAnswers",
+                column: "AnsweredFormId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormAnswers_AnsweredQuestionId",
+                table: "FormAnswers",
+                column: "AnsweredQuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormAnswers_AnsweredQuestionOptionId",
+                table: "FormAnswers",
+                column: "AnsweredQuestionOptionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormAnswers_CustomerId",
+                table: "FormAnswers",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FormAnswers_Customers_CustomerId",
+                table: "FormAnswers",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FormAnswers_QuestionForm_AnsweredFormId",
+                table: "FormAnswers",
+                column: "AnsweredFormId",
+                principalTable: "QuestionForm",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FormAnswers_QuestionOptions_AnsweredQuestionOptionId",
+                table: "FormAnswers",
+                column: "AnsweredQuestionOptionId",
+                principalTable: "QuestionOptions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FormAnswers_Questions_AnsweredQuestionId",
+                table: "FormAnswers",
+                column: "AnsweredQuestionId",
+                principalTable: "Questions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
